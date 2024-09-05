e0c98e7850306e3fd73a187905d76b0137c4cec2
RoganYeo <yic6060@gmail.com>
2024-09-06 00:08:14 +0900

    first commit

---
+++ src/main/java/com/yeo/githubactionserver/GithubActionServerApplication.java
@@ -0,0 +1,13 @@
+package com.yeo.githubactionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeo/githubactionserver/GithubActionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeo.githubactionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
