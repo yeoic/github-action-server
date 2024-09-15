9fc64aedcde30b40cfa832f26bb6a4976f1533fe
RoganYeo <yic6060@gmail.com>
2024-09-15 14:24:47 +0900

    add '!'

--- src/main/java/com/yeo/githubactionserver/HelloController.java
+++ src/main/java/com/yeo/githubactionserver/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Github Action World!";
+        return "Hello Github Action World!!";
     }
 }
