36fc743c379ef7aefe519f006d5c29b18bd51b3e
RoganYeo <yic6060@gmail.com>
2024-09-19 01:30:16 +0900

    start-server.sh 파일 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 ------------------"
+mkdir -p /home/ubuntu/github-action-server
 cd /home/ubuntu/github-action-server
 sudo fuser -k -n tcp 8080 || true
 nohup java -jar project.jar > ./output.log 2>&1 &
