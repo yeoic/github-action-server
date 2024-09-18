3a7f8df751bd8ca1245ba6d014eb2e4947b7f16e
RoganYeo <yic6060@gmail.com>
2024-09-19 01:46:49 +0900

    Error Fix 테스트

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,5 +4,6 @@ echo "--------------- 서버 배포 시작 ------------------"
 mkdir -p /home/ubuntu/github-action-server
 cd /home/ubuntu/github-action-server
 sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+JAR_FILE=$(ls *.jar)
+nohup java -jar $JAR_FILE > ./output.log 2>&1 &
 echo "--------------- 서버 배포 끝 ------------------"
