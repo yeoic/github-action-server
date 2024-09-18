3c1a23f68d5a4c9e807a96915d9614e91b0cbe9b
RoganYeo <yic6060@gmail.com>
2024-09-19 02:00:32 +0900

    start-server.sh 경로 오기입한 내용 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop github-action-server || true
 docker rm github-action-server || true
-docker pull 974087063717.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-server/github-action-server:latest
-docker run -d --name github-action-server -p 8080:8080 974087063717.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-server/github-action-server:latest
+docker pull 974087063717.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-server:latest
+docker run -d --name github-action-server -p 8080:8080 974087063717.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
